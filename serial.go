257ab12dd41f6935478805d17d6f4fc95a75409b
Eric Cooper <ecc@cmu.edu>
2016-01-22 15:08:17 -0500

    Use struct instead of casting

--- serial.go
+++ serial.go
@@ -10,41 +10,46 @@ import (
 )
 
 // A Port represents an open USB serial device.
-type Port int
+type Port struct {
+	fd int
+}
 
 // Open returns a Port for the USB serial device with the given
 // vendor and product identifiers.
 // Its behavior is undefined if more than one such USB device is present.
-func Open(vendor string, product string) (Port, error) {
+func Open(vendor string, product string) (port Port, err error) {
 	device, err := findUSB(vendor, product)
 	if err != nil {
-		return -1, err
+		return
 	}
 	const openFlags = s.O_NONBLOCK | s.O_NOCTTY | s.O_RDWR
-	fd, err := s.Open(device, openFlags, 0)
+	port.fd, err = s.Open(device, openFlags, 0)
 	if err != nil {
-		return -1, err
+		return
 	}
 	speed := uint32(s.B115200)
 	t := s.Termios{
 		Iflag: s.IGNPAR,
 		Cflag: s.CLOCAL | s.CREAD | s.CS8 | speed,
 	}
-	_, _, errno := s.Syscall6(s.SYS_IOCTL, uintptr(fd), uintptr(s.TCSETS), uintptr(unsafe.Pointer(&t)), 0, 0, 0)
+	_, _, errno := s.Syscall6(
+		s.SYS_IOCTL, uintptr(port.fd), uintptr(s.TCSETS),
+		uintptr(unsafe.Pointer(&t)), 0, 0, 0,
+	)
 	if errno != 0 {
-		return -1, errno
+		err = error(errno)
 	}
-	return Port(fd), nil
+	return
 }
 
 // Close closes the given Port.
-func (fd Port) Close() error {
-	return s.Close(int(fd))
+func (port Port) Close() error {
+	return s.Close(port.fd)
 }
 
 // Write writes len(buf) bytes from buf to Port.
-func (fd Port) Write(buf []byte) error {
-	n, err := s.Write(int(fd), buf)
+func (port Port) Write(buf []byte) error {
+	n, err := s.Write(port.fd, buf)
 	if err != nil {
 		return err
 	}
@@ -56,9 +61,9 @@ func (fd Port) Write(buf []byte) error {
 
 // Read reads from Port into buf, blocking if necessary
 // until exactly len(buf) bytes have been read.
-func (fd Port) Read(buf []byte) error {
+func (port Port) Read(buf []byte) error {
 	for off := 0; off < len(buf); {
-		n, err := s.Read(int(fd), buf[off:])
+		n, err := s.Read(port.fd, buf[off:])
 		if err != nil {
 			return err
 		}
