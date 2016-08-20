c9daf36519a3e096871c201f3a512bd6c3f3d0e4
Eric Cooper <ecc@cmu.edu>
2016-08-20 13:32:06 -0400

    Add error types to distinguish device not found from missing kernel module

--- usb.go
+++ usb.go
@@ -8,22 +8,40 @@ import (
 	"strings"
 )
 
-// hasProperty tests if the file path/property contains the given value
+// hasProperty tests if the file path/property contains the given value.
 func hasProperty(path string, property string, value string) bool {
 	v, err := ioutil.ReadFile(filepath.Join(path, property))
 	if err != nil {
 		return false
 	}
-	// compare without trailing '\n'
-	return string(v[:len(v)-1]) == value
+	if len(v) != 0 && v[len(v)-1] == '\n' {
+		// Compare without trailing '\n'
+		return string(v[:len(v)-1]) == value
+	}
+	return string(v) == value
+}
+
+type DeviceNotFoundError struct {
+	Vendor  string
+	Product string
+}
+
+func (e DeviceNotFoundError) Error() string {
+	return fmt.Sprintf("USB device %s:%s not found", e.Vendor, e.Product)
+}
+
+type TtyNotFoundError string
+
+func (e TtyNotFoundError) Error() string {
+	return fmt.Sprintf("no tty in %s: is the cdc-acm kernel module loaded?", string(e))
 }
 
 // findUSB returns the /dev/tty* path corresponding to the USB serial device
-// with the given vendor and product identifiers
+// with the given vendor and product identifiers.
 func findUSB(vendor string, product string) (path string, err error) {
 	found := false
 	device := ""
-	check := func(path string, info os.FileInfo, err error) error {
+	checkId := func(path string, info os.FileInfo, err error) error {
 		if found {
 			return filepath.SkipDir
 		}
@@ -37,18 +55,18 @@ func findUSB(vendor string, product string) (path string, err error) {
 		}
 		return nil
 	}
-	filepath.Walk("/sys/bus/usb/devices", check)
+	filepath.Walk("/sys/bus/usb/devices", checkId)
 	if !found {
-		return "", fmt.Errorf("no device with vendor %q and product %q", vendor, product)
+		return "", DeviceNotFoundError{Vendor: vendor, Product: product}
 	}
-	// filepath.Walk won't follow symlink, so expand it first
+	// filepath.Walk won't follow symlinks, so expand it first.
 	device, err = filepath.EvalSymlinks(device)
 	if err != nil {
 		return "", err
 	}
 	found = false
 	tty := ""
-	check = func(path string, info os.FileInfo, err error) error {
+	checkTty := func(path string, info os.FileInfo, err error) error {
 		if found {
 			return filepath.SkipDir
 		}
@@ -63,9 +81,9 @@ func findUSB(vendor string, product string) (path string, err error) {
 		}
 		return nil
 	}
-	filepath.Walk(device, check)
+	filepath.Walk(device, checkTty)
 	if !found {
-		return "", fmt.Errorf("no tty in %s: is the cdc-acm kernel module loaded?", device)
+		return "", TtyNotFoundError(device)
 	}
 	return filepath.Join("/dev", tty), nil
 }
