bee59513adcd968938e023a3a2bab0d3571c9b4f
Eric Cooper <ecc@cmu.edu>
2017-04-30 15:42:21 -0400

    Use pointer to Port struct

--- serial.go
+++ serial.go
@@ -15,41 +15,42 @@ type Port struct {
 	fd int
 }
 
-// Open returns a Port for the USB serial device with the given
-// vendor and product identifiers.
+// Open opens the USB serial device with the given vendor and product identifiers.
 // Its behavior is undefined if more than one such USB device is present.
-func Open(vendor string, product string) (port Port, err error) {
+func Open(vendor string, product string) (*Port, error) {
 	device, err := findUSB(vendor, product)
 	if err != nil {
-		return
+		return nil, err
 	}
 	const openFlags = unix.O_NONBLOCK | unix.O_NOCTTY | unix.O_RDWR
-	port.fd, err = unix.Open(device, openFlags, 0)
+	fd, err := unix.Open(device, openFlags, 0)
 	if err != nil {
-		return
+		return nil, err
 	}
 	speed := uint32(unix.B115200)
 	t := unix.Termios{
 		Iflag: unix.IGNPAR,
 		Cflag: unix.CLOCAL | unix.CREAD | unix.CS8 | speed,
 	}
-	_, _, errno := unix.Syscall6(
-		unix.SYS_IOCTL, uintptr(port.fd), uintptr(unix.TCSETS),
-		uintptr(unsafe.Pointer(&t)), 0, 0, 0,
-	)
+	_, _, errno := unix.Syscall6(unix.SYS_IOCTL, uintptr(fd),
+		uintptr(unix.TCSETS), uintptr(unsafe.Pointer(&t)), 0, 0, 0)
 	if errno != 0 {
-		err = error(errno)
+		return nil, error(errno)
 	}
-	return
+	err = unix.SetNonblock(fd, false)
+	if err != nil {
+		return nil, err
+	}
+	return &Port{fd}, nil
 }
 
 // Close closes the given Port.
-func (port Port) Close() error {
+func (port *Port) Close() error {
 	return unix.Close(port.fd)
 }
 
 // Write writes len(buf) bytes from buf to port.
-func (port Port) Write(buf []byte) error {
+func (port *Port) Write(buf []byte) error {
 	n, err := unix.Write(port.fd, buf)
 	if err != nil {
 		return err
@@ -62,7 +63,7 @@ func (port Port) Write(buf []byte) error {
 
 // Read reads from port into buf, blocking if necessary
 // until exactly len(buf) bytes have been read.
-func (port Port) Read(buf []byte) error {
+func (port *Port) Read(buf []byte) error {
 	for off := 0; off < len(buf); {
 		n, err := unix.Read(port.fd, buf[off:])
 		if err != nil {
